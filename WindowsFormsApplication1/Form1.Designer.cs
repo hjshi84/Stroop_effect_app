edfbded51e9f3f36707095a4f755bcb158eb55c3
Huangjun Shi <hjshi84@outlook.com>
2016-05-08 22:31:48 +0800

    change nothing

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox2.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(522, 246);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(536, 278);
+            this.tabControl1.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.pictureBox2);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(528, 252);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "颜色字体";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.pictureBox1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(528, 252);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "数字长";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(522, 246);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.pictureBox3);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(528, 252);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "背景字体";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox3.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(522, 246);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.trackBar2.AutoSize = false;
+            this.trackBar2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.trackBar2.Location = new System.Drawing.Point(222, 281);
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(123, 21);
+            this.trackBar2.TabIndex = 5;
+            this.trackBar2.Value = 5;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(175, 285);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "间隔：";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 284);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "字体：";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.trackBar1.AutoSize = false;
+            this.trackBar1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.trackBar1.Location = new System.Drawing.Point(46, 280);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(123, 21);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.Value = 5;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(365, 285);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 12);
+            this.label3.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(536, 309);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "斯特鲁普测试";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tabPage3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label3;
+
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Timers;
+
+namespace WindowsFormsApplication1
+{
+  
+    public partial class Form1 : Form
+    {
+        string[] word=  { "红","黄","蓝","绿","黑" };
+        SolidBrush[] color = { new SolidBrush(Color.Red), new SolidBrush(Color.Yellow), new SolidBrush(Color.Blue), new SolidBrush(Color.Green), new SolidBrush(Color.Black) };
+        Color[] bgcolor = { Color.Red, Color.Yellow ,Color.Blue,Color.Green,Color.Black};
+        string textstring = "";
+
+
+        Graphics g1,g2,g3;
+
+        Bitmap aaa,bbb,ccc;
+        
+        System.Timers.Timer abc;
+        
+
+        Random ra = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+            abc = new System.Timers.Timer();
+            abc.Interval = 200*int.Parse(trackBar2.Value.ToString())+500;
+            label3.Text = "时间间隔为: " + abc.Interval/1000+"s";
+            abc.Elapsed+=apple;
+            abc.Start();
+        }
+
+        public void apple(object source, System.Timers.ElapsedEventArgs e)
+        {
+           
+            
+
+                switch (tabControl1.SelectedIndex)
+                {
+                    case 0:
+                        pictureBox2.Image = randomdrawcolor();
+                        break;
+                    case 1:
+                        pictureBox1.Image = randomdrawint();
+                        break;
+                    case 2:
+                        pictureBox3.Image = randomdrawbg();
+                        break;
+                    default:
+                        break;
+                }
+               
+            
+        }
+
+        public Bitmap randomdrawint()
+        {
+            //aaa.Dispose();
+           // if (aaa != null) 
+           //     aaa.Dispose();
+           // if (g2 != null) g2.Dispose();
+            aaa = new Bitmap(pictureBox3.Size.Width, pictureBox3.Size.Height);
+            g1 = Graphics.FromImage(aaa);
+            string string1 = ra.Next(10).ToString();
+            int number = ra.Next(6)+1;
+            for (int i = 0; i < number; i++)
+            {
+                textstring = string1 + textstring;
+            }
+                g1.DrawString(textstring, new Font("Arial", 
+                    72 + 9 * int.Parse(trackBar1.Value.ToString())
+                   ), color[ra.Next(5)], new PointF(pictureBox3.Size.Width / 2 - (72 + 9 * int.Parse(trackBar1.Value.ToString()))*(number/2), pictureBox3.Size.Height / 2 - (72 + 9 * int.Parse(trackBar1.Value.ToString()))));
+                number = 0;
+                textstring = "";
+                
+                return aaa;
+        }
+
+        public Bitmap randomdrawbg()
+        {
+            //aaa.Dispose();
+           // if (aaa != null) aaa.Dispose();
+           // if (g2 != null) g2.Dispose();
+            
+            bbb = new Bitmap(pictureBox3.Size.Width, pictureBox3.Size.Height);
+            
+            g2 = Graphics.FromImage(bbb);
+            int choosebgcolor = ra.Next(5);
+            g2.Clear(bgcolor[choosebgcolor]);
+            int choosecolor=ra.Next(5);
+            while(true)
+            {
+                if (choosecolor!=choosebgcolor) 
+                {
+                    break;
+                }
+                else 
+                {
+                    choosecolor=ra.Next(5);
+                }
+            }
+            g2.DrawString(word[ra.Next(5)], new Font("Arial", 72 + 18 * int.Parse(trackBar1.Value.ToString())), color[choosecolor], new PointF(
+                (pictureBox3.Size.Width / 2 - (72 + 18 * int.Parse(trackBar1.Value.ToString())))>0? pictureBox3.Size.Width / 2 - (72 + 18 * int.Parse(trackBar1.Value.ToString())):0
+                , 
+                (pictureBox3.Size.Height / 2 - (72 + 18 * int.Parse(trackBar1.Value.ToString())))>0?pictureBox3.Size.Height / 2 - (72 + 18 * int.Parse(trackBar1.Value.ToString())):0
+                ));
+            
+            return bbb;
+        }
+
+        public Bitmap randomdrawcolor()
+        {
+            //aaa.Dispose();
+           // if (aaa != null) aaa.Dispose();
+           // if (g3 != null) g2.Dispose();
+            ccc = new Bitmap(pictureBox3.Size.Width, pictureBox3.Size.Height); 
+            
+            g3 = Graphics.FromImage(ccc);
+
+            g3.DrawString(word[ra.Next(5)], new Font("Arial", 188 + 27 * int.Parse(trackBar1.Value.ToString())), color[ra.Next(5)], new PointF(
+                (pictureBox3.Size.Width / 2 - (188 + 27 * int.Parse(trackBar1.Value.ToString())))>0?pictureBox3.Size.Width / 2 - (188 + 27 * int.Parse(trackBar1.Value.ToString())):0
+                , (pictureBox3.Size.Height / 2 - (188 + 27 * int.Parse(trackBar1.Value.ToString())))>0?pictureBox3.Size.Height / 2 - (188 + 27 * int.Parse(trackBar1.Value.ToString())):0
+                ));
+            
+            return ccc;
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            abc.Interval = 200 * int.Parse(trackBar2.Value.ToString()) + 500;
+            label3.Text = "时间间隔为: " + abc.Interval/1000+"s";
+        }
+
+        
+    }
+}
